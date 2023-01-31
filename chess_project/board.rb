ab5f3d28cd49914bd625ce341c74eb44f497c5aa
Michael Gastello <michaelgastello@aol.com>
2023-01-31 15:23:17 -0500

    initialized board

---
+++ chess_project/board.rb
@@ -0,0 +1,11 @@
+class Board
+    def initialize
+        @grid = Array.new(8) { Array.new(8) }   # insert null piece
+        # (2..5).to_a.each do |idx1|
+        #     @grid[idx1].each_with_index do |ele, idx2|
+        #         @grid[idx1][idx2] = # instance of null piece
+        #     end
+        # end
+    end
+end
+
